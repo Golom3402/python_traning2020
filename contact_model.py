db15c421a622ea9198cc1423167f7f2baf83de46
Golom3402 <40380915+Golom3402@users.noreply.github.com>
2020-06-11 16:32:45 +0200

    add new test module for add contacts. refactor Katalon scenario to add new contact. added contact model module, with Contact class and description her attributes.

---
+++ contact_model.py
@@ -0,0 +1,33 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, photo, title,
+                 company, address, home_tel, mobile_tel, work_tel, fax, email, email2, email3,
+                 homepage, b_day, b_month, b_year, anny_day, anny_month, anny_year,
+                 group, address2, phone2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.photo = photo
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_tel = home_tel
+        self.mobile_tel = mobile_tel
+        self.work_tel = work_tel
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.b_day = b_day
+        self.b_month = b_month
+        self.b_year = b_year
+        self.anny_day = anny_day
+        self.anny_month = anny_month
+        self.anny_year = anny_year
+        self.group = group
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+
