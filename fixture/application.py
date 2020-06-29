b5d45bbeb3bbe758c2d8ac18472242e31ff69caa
Golom3402 <40380915+Golom3402@users.noreply.github.com>
2020-06-30 01:40:50 +0200

    оптимизировал методы переходов между страницами, удалил wd.implicity_wait(5) из Application

--- fixture/application.py
+++ fixture/application.py
@@ -10,7 +10,6 @@ from fixture.session import SessionHelper
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -25,7 +24,8 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        if not (wd.current_url.endswith("addressbook/") and len(wd.find_elements_by_name('MainForm')) > 0):
+            wd.get("http://localhost/addressbook/")
 
     def return_to_home_page(self):
         wd = self.wd
