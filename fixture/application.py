2d29aadc4912f95ae7cd4fd7506f23e1114b0931
danil.golomentsev <danil.golomentsev@gs-labs.ru>
2020-07-22 09:48:21 +0200

    добавил опции командной строки для типа браузера и  адреса тестируемого приложения

--- fixture/application.py
+++ fixture/application.py
@@ -12,11 +12,19 @@ from fixture.session import SessionHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox()
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == 'ie':
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError(f"Unrecognized browser {browser}")
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -30,7 +38,7 @@ class Application:
         wd = self.wd
 
         if not (wd.current_url.endswith("addressbook/") and len(wd.find_elements_by_name('MainForm')) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def return_to_home_page(self):
         wd = self.wd
