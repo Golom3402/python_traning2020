e0e12fd10a8b67cdf8f7da9ecf2a36d48271acb4
Golom3402 <40380915+Golom3402@users.noreply.github.com>
2020-07-29 01:04:14 +0200

    реализована загрузка тестовых данных из файлов в формате JSON и параметризация тестов через внедрение этих тестовых данных в фикстуру с помощью pytest_generate_tests для восстановления структуры объектов из JSONфайла использована библиотека jsonpickle

--- tests/test_contact_add.py
+++ tests/test_contact_add.py
@@ -3,18 +3,9 @@ import pytest
 from model.contact_model import Contact
 
 
-def test_add_new_contact(app, data_contacts):
-    contact = data_contacts
+def test_add_new_contact(app, json_contacts):
+    contact = json_contacts
     old_list = app.contact.get_contact_list()
-    # contact = Contact(first_name='Test_user1', middle_name='Петрович', last_name="Ватутин", nickname="Test1",
-    #                   photo="C:\\fakepath\\Koala.jpg", title='TutleTest data1', company='E-corp',
-    #                   address='York sheer 13',
-    #                   home_tel='+3-9-99-99-9-9-9', work_tel='+7-495-399-59-59', mobile_tel='+79291929929',
-    #                   fax='3334528',
-    #                   email='petrovi4@mail.ru', email2='test1@yandex.ru', email3='vatutin123@gmail.com',
-    #                   homepage='www.facebook.com', b_day='14', b_month='December', b_year='1944', anny_day='14',
-    #                   anny_month='April', anny_year='1994', group=None, address2='address 2 2 2', phone2='123456789-0',
-    #                   notes='notes notes notes notes notes notes notes notes notes notes notes notes notes notes')
     app.open_home_page()
     app.contact.add_new(contact=contact)
     app.return_to_home_page()
--- tests/test_group_add.py
+++ tests/test_group_add.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
-import string
-from sys import maxsize
-import pytest
-import random
-import string
+
 from model.group_model import Group
-from data.groups import testdata
 
 
-def test_add_new_group(app, data_groups):
-    group = data_groups
+def test_add_new_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     # group = Group(name="first_group", header="GROUP", footer="footer group")
     app.group.create(group)
@@ -17,16 +12,6 @@ def test_add_new_group(app, data_groups):
     new_group = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_group, key=Group.id_or_max)
-
-
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_group = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_group)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_group, key=Group.id_or_max)
 
 
 
